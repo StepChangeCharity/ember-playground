aae707e7b510a6e2b3f15bc17610fdae184fa060
dgkane <dave.kane@hotmail.co.uk>
2015-10-22 10:47:01 +0100

    Comment

--- app/components/user-details.js
+++ app/components/user-details.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	// set css class(es) for component
+	// css in app/stlyes applied by default
 	classNames: ['css-details-panel'],
 });
